d8843d29a5ea668e8f8ac6101d130f2855f52c39
Rafael Suleymenov <rafaelsuleymenov5@gmail.com>
2025-07-01 19:38:15 +0500

    version 0.0.0.1

---
+++ src/main/java/kaspi/kz/car_service_station/CarServiceStationApplication.java
@@ -0,0 +1,13 @@
+package kaspi.kz.car_service_station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaspi/kz/car_service_station/CarServiceStationApplicationTests.java
@@ -0,0 +1,13 @@
+package kaspi.kz.car_service_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
